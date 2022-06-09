c0b8f7a57a156640fbc323251ccf0c21234315f0
seomy <muyear.seo@gmail.com>
2022-06-09 19:51:54 +0900

    ExceptionResolver @ResponseStatus

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,8 +1,10 @@
 package hello.exception.api;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
@@ -13,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class ApiExceptionController {
-	
+
 	@GetMapping("/api/members/{id}")
 	public MemberDto getMember(@PathVariable("id") String id) {
 		if (id.equals("ex")) {
@@ -27,12 +29,17 @@ public class ApiExceptionController {
 		}
 		return new MemberDto(id, "hello " + id);
 	}
-	
+
 	@GetMapping("/api/response-status-ex1")
 	public String responseStatusEx1() {
 		throw new BadRequestException();
 	}
-	
+
+	@GetMapping("/api/response-status-ex2")
+	public String responseStatusEx2() {
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+	}
+
 	@Data
 	@AllArgsConstructor
 	static class MemberDto {
